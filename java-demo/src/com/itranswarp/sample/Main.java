e6c369765124d1879850b2483a4051f41d8e4993
李美旺 <17803125899@163.com>
2021-04-13 21:39:44 +0800

    java demo

---
+++ java-demo/src/com/itranswarp/sample/Main.java
@@ -0,0 +1,7 @@
+package com.itranswarp.sample;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ java-demo/src/com/itranswarp/world/Person.java
@@ -0,0 +1,7 @@
+package com.itranswarp.world;
+
+public class Person {
+    public static void main(String[] args) {
+        
+    }
+}
